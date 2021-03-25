f76a029d117a58f6c351db27a4fd4dd9e723e76b
DmitrySkripkin <sepia13@yandex.ru>
2021-03-25 19:14:42 +0300

    Create sample notif

--- android/src/main/java/com/hubhead/plugins/notification/Notification.java
+++ android/src/main/java/com/hubhead/plugins/notification/Notification.java
@@ -5,7 +5,9 @@ import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import android.util.Log;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.app.NotificationChannel;
 
 @NativePlugin
 public class Notification extends Plugin {
@@ -13,9 +15,19 @@ public class Notification extends Plugin {
     @PluginMethod
     public void echo(PluginCall call) {
         String value = call.getString("value");
-         Log.i("PLUGIN","EEEEEEEEEEEE");
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+
+        Notification notification = new NotificationCompat.Builder(this, "0")
+                    .setContentTitle("Title")
+                    .setContentText("Body")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .build();
+            NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(
+                        "0", "0", NotificationManager.IMPORTANCE_DEFAULT);
+                notificationManager.createNotificationChannel(channel);
+            }
+            manager.notify("tag", 0, notification);
     }
 }
