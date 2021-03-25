4ec9bf872c7a95f93e56997af2425bcbda21b36c
DmitrySkripkin <sepia13@yandex.ru>
2021-03-25 19:01:07 +0300

    Testing commit, no idea what im doing

--- android/src/main/java/com/hubhead/plugins/notification/Notification.java
+++ android/src/main/java/com/hubhead/plugins/notification/Notification.java
@@ -5,6 +5,7 @@ import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import android.util.Log;
 
 @NativePlugin
 public class Notification extends Plugin {
@@ -12,7 +13,7 @@ public class Notification extends Plugin {
     @PluginMethod
     public void echo(PluginCall call) {
         String value = call.getString("value");
-
+         Log.i("PLUGIN","EEEEEEEEEEEE");
         JSObject ret = new JSObject();
         ret.put("value", value);
         call.success(ret);
